f89c0f689b3b7c41fc3eec76a43fe18945021e07
--replace-all <ethan.s.dean625@gmail.com>
2026-02-27 09:54:42 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+You know why you are here. You know who you are. What you did. You know what this is for. You know. And I know too.
+You have been warned. When I find you, and I will find you, there shall be no escape.
---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log("Connection complete. Virus installation underway.");
+console.log("Virus installation complete.");
+console.log(
+  "Now I'm in. I'm one step closer to finding you. There is no escape.",
+);
